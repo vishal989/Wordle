1ff0b7890d9639bbd34a2caa9d3ccf584efebba5
vishal989 <vavishalarya@gmail.com>
2022-10-31 11:50:20 +0530

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,38 @@
-import {observer, useLocalObservable} from 'mobx-react-lite'
+import { observer, useLocalObservable } from 'mobx-react-lite'
 import { useEffect } from 'react'
-import Qwerty from '../components/Qwerty'
 import Guess from '../components/Guess'
+import Querty from '../components/Qwerty'
 import PuzzleStore from '../stores/PuzzleStore'
 
 export default observer(function Home() {
-    const store = useLocalObservable(() => PuzzleStore)
-    useEffect(() => {
-        store.init()
-        window.addEventListener('keyup', store.handleKeyup)
+  const store = useLocalObservable(() => PuzzleStore)
+  useEffect(() => {
+    store.init()
+    window.addEventListener('keyup', store.handleKeyup)
 
-        return () => {
-            window.removeEventListener('keyup', store.handleKeyup)
-        }
-    }, [])
-
-    return <div className= "flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
-        <h1 className = "text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle</h1>
-        {store.guesses.map((_, i) => (
-            <Guess 
-                key = {i} 
-                word={store.word} 
-                guess={store.guesses[i]} 
-                isGuessed={i < store.currentGuess} 
-            />
-        ))} 
-        {store.won && <h1>You won!</h1>}
-        {store.lost && <h1>You lost!</h1>}
-        {(store.won || store.lost) && (
-            <button onClick={store.init}>Play Again</button>
-        )}
-        <Qwerty store={store}/>
-        word: {store.word}
-        guesses: {JSON.stringify(store.guesses)}
+    return () => {
+      window.removeEventListener('keyup', store.handleKeyup)
+    }
+  }, [])
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
+      <h1 className="bg-gradient-to-br from-blue-400 to-green-400 bg-clip-text text-6xl font-bold uppercase text-transparent">
+        Wordle
+      </h1>
+      {store.guesses.map((_, i) => (
+        <Guess
+          key={i}
+          word={store.word}
+          guess={store.guesses[i]}
+          isGuessed={i < store.currentGuess}
+        />
+      ))}
+      {store.won && <h1>You won!</h1>}
+      {store.lost && <h1>You lost!</h1>}
+      {(store.won || store.lost) && (
+        <button onClick={store.init}>Play Again</button>
+      )}
+      <Querty store={store} />
     </div>
+  )
 })
